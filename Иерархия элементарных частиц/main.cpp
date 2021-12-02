89fbc3d607e808c6f31c473cccc29e6e429ce46a
Ivetta46 <79827292+Ivetta46@users.noreply.github.com>
2021-12-02 14:14:35 +0300

    Add files via upload

---
+++ Иерархия элементарных частиц/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "Particles.h"
+
+
+double Particle::fromEVtoJ(double e) {
+    return e * 1.602176634E-19;
+}
+
+void Fermion::printInfo() const {
+    std::cout << getName() << endl;
+    std::cout << "Refers to fermions (particles with a half-integer spin value)" << endl;
+    if (isStable) {
+        std::cout << "Stable" << endl;
+    }
+    else {
+        std::cout << "Unstable, lifetime: " << timeOfLife << " c" << endl;
+    }
+    if (hasMass) {
+        std::cout << "The mass is equal to " << E0 << " EV/c^2" << endl;
+    }
+    else {
+        std::cout << "Has no mass" << endl;
+    }
+    std::cout << "Spin: " << (spin > 0 ? to_string(spin) : "") << " 1/2" << endl;
+    printElCharge();
+}
+
+void Quark::printInfo() const {
+    Fermion::printInfo();
+    std::cout << "Smell: " << FlavorsNames[f] << "-quark" << endl;
+}
+
+void Lepton::printInfo() const {
+    Fermion::printInfo();
+}
+
+void Boson::printInfo() const {
+    std::cout << getName() << endl;
+    std::cout << "Refers to bosons (a particle with an integer spin value)" << endl;
+    if (isStable) {
+        std::cout << "Stable" << endl;
+    }
+    else {
+        std::cout << "Unstable, lifetime: " << timeOfLife << " c" << endl;
+    }
+    if (hasMass) {
+        std::cout << "The mass is equal to " << E0 << " эВ/c^2" << endl;
+    }
+    else {
+        std::cout << "Has no mass" << endl;
+    }
+    std::cout << "Spin: " << spin << endl;
+    std::cout << "Electric charge: " << elCharge << " elementary electric charges" << endl;
+}
+
+void Foton::printInfo() const {
+    Boson::printInfo();
+}
+
+void Electron::printInfo() const {
+    Lepton::printInfo();
+}
+
+
+int main() {
+    Particle **particles = new Particle*[5];
+    particles[0] = new Electron();
+    particles[1] = new Quark(2, 173.1E9, 5E-25, Quark::t);
+    particles[2] = new Foton();
+    particles[3] = new Quark(-1, 4.67E9, 1E-12, Quark::b);
+    particles[4] = new Boson(false, true, 0, 0, 125E9, 1.56E-22);
+
+    // выводим информацию
+    for (int i = 0; i < 5; i++) {
+
+        particles[i]->printInfo();
+
+        std::cout << "Weight, kg: " << particles[i]->getMass() << endl;
+    }
+
+
+    for (int i = 0; i < 5; i++) {
+        delete particles[i];
+    }
+    delete [] particles;
+    return 0;
+}
